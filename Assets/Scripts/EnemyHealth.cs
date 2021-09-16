dd3c05495e0a36f7e031b909d8b779c965eca9e8
LeviL1 <78377282+LeviL1@users.noreply.github.com>
2021-09-16 17:46:59 -0500

    Updating enemy health script

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,26 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Enemyhealth deals with taking damage and death
 public class EnemyHealth : MonoBehaviour
 {
   public float health = 100;
-  private Animator _anim;
+  private Animator _anim; //_anim will be enemies animator
     // Start is called before the first frame update
     void Start()
     {
-    _anim = this.GetComponent<Animator>();
+    _anim = this.GetComponent<Animator>(); //get the animator component
     }
 
     // Update is called once per frame
     void Update()
     {
-    if (health == 0) { Die(); }
+    if (health == 0) { Die(); } //if health == 0 then die
     }
-  public void TakeDamage(float dmg) { health -= dmg; }
+  public void TakeDamage(float dmg) { health -= dmg; } //subtracts incoming dmg from health
+
+//Kills enemy and displays animation
   public void Die() 
   {
     
-    _anim.Play("New Animation 0");
-    Destroy(this.gameObject, 1.25f);
+    _anim.Play("New Animation 0"); //play enemydeath animation
+    Destroy(this.gameObject, 1.25f); //destroy obj after animation
   }
 }
