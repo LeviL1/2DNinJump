ac8b94328b490de6768afeb2eb11e4562375aed0
LeviL1 <78377282+LeviL1@users.noreply.github.com>
2021-09-16 17:39:00 -0500

    Updating DontDestroy Script

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+//Dont destroy keeps music from being destroyed between scenes
 public class DontDestroy : MonoBehaviour
 {
-  private GameObject[] music;
+  private GameObject[] _music; //array of gameobjs
   private void Awake()
   {
 
-    DontDestroyOnLoad(this.gameObject);
+    DontDestroyOnLoad(this.gameObject); //Dont destroy music upon loading new scenes
   }
   private void Update()
   {
-    music = GameObject.FindGameObjectsWithTag("Music");
+    _music = GameObject.FindGameObjectsWithTag("Music"); //find all music objs and fill _music with them
     
-    if (music.Length > 1) { Destroy(music[1]); }
+    if (_music.Length > 1) { Destroy(_music[1]); } //if more than one music player exists delete the additional one
   }
 }
