60278082e9bcee13bfe6e40c35bcee696a76d32b
LeviL1 <78377282+LeviL1@users.noreply.github.com>
2021-09-16 17:57:09 -0500

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  public CharacterController2D controller;
-  public float runSpeed = 40;
-  float horizontalMove = 0f;
-  public GameObject shuriken;
-  public Animator anim;
-  bool jump = false;
+  public CharacterController2D controller; //CC attached to the player
+  public float runSpeed = 40; //runspeed for the player
+  float horizontalMove = 0f; //the value for horizontal movement
+  public GameObject shuriken; //obj the player throws
+  public Animator anim; //player animator component
+  bool jump = false; //jump = false by default
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +19,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if player is moving play runanimation
     if (horizontalMove != 0)
     {
       anim.Play("runanim");
     }
+    //if player is not moving play idle anim
     else { anim.Play("Idle Ninja"); }
 
 
-    horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
+    horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //positive if moving right negative if moving left
+        //if space is pressed
     if (Input.GetButtonDown("Jump"))
     {
-      jump = true;
+      jump = true; 
     }
+    //if f is pressed throw a shuriken
     if (Input.GetKeyDown(KeyCode.F)) 
     {
       Instantiate(shuriken, transform.position, transform.rotation);
